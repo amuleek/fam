ab31f25d677474e6ebb729a414b37992f4e3ec2a
amuleek <amuleeksidhu96@gmail.com>
2019-05-17 21:15:56 +0530

    two

--- mom.cpp
+++ mom.cpp
@@ -10,4 +10,22 @@ using std::cin;
 using std::endl;
 using std::vector;
 using std::list;
+typedef long long int long_int;
+const int max_int = 1000000001; // value, that could't be in the table. Analog of NULL.
+
+// function for calculation of hash    
+inline int hash(long_int a_prime, long_int b_prime, int p_prime, int table_size, int key)
+{
+    return (((a_prime * key + b_prime) % p_prime) % table_size);
+}
+
+// class for mini-hash table in cells of main hash-table 
+class Bucket
+{
+    vector<int> _cells;
+    int size; // the size of mini-table should be greater then 4 
+    long_int hash_a;
+    long_int hash_b;
+    int prime;
+
 
