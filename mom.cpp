2c3b7c2eef43be631992c8b3e42fd79c480fbbb9
amuleek <amuleeksidhu96@gmail.com>
2019-05-17 21:13:42 +0530

    one

---
+++ mom.cpp
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stdio.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::list;
+
